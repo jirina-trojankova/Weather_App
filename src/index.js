8d947c3348d4d804a02dcf1c3fb8c3c8ba4e7ac7
jirina-trojankova <jirina.trojankova@seznam.cz>
2018-06-30 19:43:22 +0200

    Shows random users too

--- src/index.js
+++ src/index.js
@@ -3,7 +3,32 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 class WheatherApp extends React.Component {
-   render() {
+    constructor() {
+        super();
+        this.state = {
+            pictures: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://randomuser.me/api/?results=5')
+    //fetch('//http://api.openweathermap.org/data/2.5/forecast?lat=49.9415967&lon=14.3316786&appid=ed62e370682cc9e4144620905eff37e4')
+    .then(results => {
+        return results.json();
+    }).then(data => {
+    let pictures = data.results.map((pic) =>{
+        return(
+            <div key={pic.results}>
+                <img src={pic.picture.medium} alt=""/>
+            </div>
+        )
+    })
+    this.setState({pictures: pictures});
+    console.log('state', this.state.pictures);
+})
+}    
+
+    render() {
        return(
         <React.Fragment>
             <div className="row">
@@ -14,6 +39,9 @@ class WheatherApp extends React.Component {
                 <Day day="Friday"/>
                 <Day day="Saturday"/>
                 <Day day="Sunday"/>
+            </div>
+            <div className="row">
+                {this.state.pictures}
             </div>
         </React.Fragment>
        );
@@ -34,6 +62,3 @@ ReactDOM.render(
         <WheatherApp />,
         document.getElementById('root')
 );
-
-//http://api.openweathermap.org/data/2.5/forecast?lat=49.9415967&lon=14.3316786&appid=ed62e370682cc9e4144620905eff37e4
-
