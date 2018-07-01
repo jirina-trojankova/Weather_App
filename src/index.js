ed5895240964b821868f9e585038a3ae02ca9ed7
jirina-trojankova <jirina.trojankova@seznam.cz>
2018-07-01 13:06:47 +0200

    console.logs connection of Weather

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-class WheatherApp extends React.Component {
-       
+class WheatherApp extends React.Component { 
     render() {
        return(
         <React.Fragment>
@@ -32,7 +31,6 @@ class Moderator extends React.Component {
 
     componentDidMount() {
         fetch('https://randomuser.me/api/?results=1')
-    //fetch('//http://api.openweathermap.org/data/2.5/forecast?lat=49.9415967&lon=14.3316786&appid=ed62e370682cc9e4144620905eff37e4')
     .then(results => {
         return results.json();
     }).then(data => {
@@ -59,11 +57,41 @@ class Moderator extends React.Component {
 class Day extends React.Component {
     render() {
         return(
-            <div className="day">It's.. {this.props.day}</div>
+            <div className="day">
+            <div>It's.. {this.props.day}</div>
+            <Weather />
+            </div>
         );
     }
 }
 
+class Weather extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('//api.openweathermap.org/data/2.5/forecast?lat=49.9415967&lon=14.3316786&appid=ed62e370682cc9e4144620905eff37e4')
+        .then((Response) => Response.json())
+        .then((findresponse) => {
+            console.log(findresponse);
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                Hi, I am weather..
+                {this.state.data.map((dynamicData,i) => 
+                <div></div>)
+                }
+            </div>
+        );
+    }
+}
 
 
 ReactDOM.render(
